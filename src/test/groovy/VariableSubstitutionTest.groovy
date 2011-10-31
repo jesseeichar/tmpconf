394794c231c783e8d5b5cdaa75dfaeefe077b87c
Jesse Eichar <jesse.eichar@camptocamp.com>
2011-10-31 08:44:30 +0100

    fixed test

--- src/test/groovy/VariableSubstitutionTest.groovy
+++ src/test/groovy/VariableSubstitutionTest.groovy
@@ -1,5 +1,9 @@
+/**
+ * This is ran after the configuration files have been processed and are in the output directory.  It
+ * verifies that all variables have been correctly substituted
+ */
 public class VariableSubstitutionTest {
-	def ignorables = [".png", ".gif", ".ico", ".bmp", ".jpg", ""]
+	def ignorables = [".png", ".gif", ".ico", ".bmp", ".jpg", ".odg", ".pdf"]
 	/**
 	 * @param project The maven project.  you can get all information about the project from this object
 	 * @param log a logger for logging info
@@ -8,25 +12,30 @@ public class VariableSubstitutionTest {
 	 * @param target the server property which is normally set by the build profile.  It indicates the project that is being built
 	 * @param classifier the "classifier" that the project is being deployed to.  For example integration or production
 	 * @param targetDir a File object referencing the targetDir
-	 * @param buildSupportDir a File object referencing the build_support dir of the target project
-	 * @param outputDir the directory to copy the generated configuration files to
+	 * @param buildSupportDir a File object referencing to the build_support dir of the target project
+	 * @param outputDir The File object referencing to the directory containing all the processed configuraiton files
 	 */
 	def execute(def project, def log, def ant, def basedirFile, 
 							def target, def classifier, def targetDir, 
 							def buildSupportDir, def outputDir) {
-		new File(classesDir).eachFileRecurse{ file ->
+		def failures = []
+		outputDir.eachFileRecurse{ file ->
 			def extIndex = file.name.lastIndexOf(".")
 			def ignorable = file.name.equals("favicon")
 			if(extIndex > -1) {
 				def ext = file.name.substring(extIndex)
-				println(ext)
 				ignorable = ignorables.contains(ext)
 			}
 
 			if(file.isFile() && !ignorable) {
 				def m = file.getText("UTF-8") =~ /@\S*@/
-				assert !m, "${m[0]} was found as an unmatched variable in ${file.path.substring(classesDir.length())}.  Define the variable in a shared.maven.filters"
+				if(m.find()) {
+					failures << "${m[0]} was found as an unmatched variable in ${file.path.substring(outputDir.path.length())}.  Define the variable in a shared.maven.filters"
+				}
 			}
+		}
+		if(!failures.isEmpty()) {
+			throw new AssertionError(failures.join("\n"))
 		}
 	}
 }
